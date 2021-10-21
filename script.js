5388e92c7756451edd3bfdf278490d50fc561eca
Bernardanxiety <beniek12@onet.pl>
2021-10-21 08:19:08 -0700

    Fixed close button

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const closeBtn = document.querySelector('.close-icon');
 const socials = document.querySelector('.socials');
 
 openBtn.addEventListener('click', function() {
-    socials.classList.add('enabled');
+    socials.classList.toggle('enabled');
+    openBtn.classList.toggle('open-icon-close');
 })
 
 closeBtn.addEventListener('click', function() {
